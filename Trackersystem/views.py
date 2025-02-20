6e0f6d9099c0e00d97a5803a9570b088ae225128
Rita Akankunda <akankundarita04@gamil.com>
2025-02-20 13:04:39 +0300

    initial commit

--- Trackersystem/views.py
+++ Trackersystem/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home_page_view(request):
-    return HttpResponse("Hello, World")
+    return HttpResponse("Homepage")
+
+def about_page_view(request):
+    context = {
+        "name": "Rita",
+        "age": 21,
+        }
+    return render(request, "Trackersystem/about.html", context)                
+
     
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -34,12 +35,13 @@ INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
-    "django.contrib.sessions",
+    "django.contrib.sessions",       
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "Trackersystem",
+    "rest_framework",
 ]
-
+  
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -117,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = (
+    os.path/join(BASE_DIR / "static",)
+)
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
